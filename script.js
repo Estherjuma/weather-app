e92127ccb09c7550392b2a906bf45e0dc0d50236
Estherjuma <153985400+Estherjuma@users.noreply.github.com>
2024-01-12 22:11:33 +0300

    added the search functionality using the API

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+function updateWeather(response) {
+    let temperatureElement = document.querySelector("#weather-app-temp");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#weather-app-city")
+    temperatureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = response.data.city
+}
+;
+
+function searchCity(city) {
+    let apiKey = "15b046dc320ab53a013bbof2tfa365eb";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeather);
+}
+
+
 function handleSearch(event) {
     event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input")
-    let cityElement = document.querySelector("#weather-app-city")
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#weather-app-city");
     cityElement.innerHTML =searchInput.value;
+    searchCity(searchInput.value)
 }
-let searchFormElement = document.querySelector("#search-form")
-searchFormElement.addEventListener("submit", handleSearch)
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Nairobi");
